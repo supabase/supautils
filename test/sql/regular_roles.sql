fe6b3e4ab89a28de1acd85b0835bac21332d3b02
steve-chavez <stevechavezast@gmail.com>
2021-05-12 00:36:37 -0500

    refactor: \echo for better spacing on the tests
    Otherwise they're hard to read

--- test/sql/regular_roles.sql
+++ test/sql/regular_roles.sql
@@ -1,11 +1,9 @@
 -- tests to make sure our hooks don't mess regular roles functionality
 alter role fake rename to new_fake;
-
 alter role new_fake createrole createdb;
-
 drop role new_fake;
+create role non_reserved;
+\echo
 
 -- ensure we don't call the hook for invalid roles
 drop role public;
-
-create role non_reserved;
--- test/sql/reserved_roles.sql
+++ test/sql/reserved_roles.sql
@@ -1,32 +1,40 @@
 -- cannot rename an existing role to a reserved role
 alter role fake rename to reserved_but_not_yet_created;
+\echo
 
 -- cannot rename a reserved role
 alter role supabase_storage_admin rename to another;
+\echo
 
 -- cannot alter-options a reserved role
 alter role supabase_storage_admin nologin superuser;
 alter role supabase_storage_admin password 'pass';
+\echo
 
 -- cannot alter-config for a reserved role
 alter role supabase_storage_admin set search_path to 'test';
+\echo
 
 -- cannot bypass alter-options check by using current_user
 set role anon;
 alter role current_user password 'pass';
 reset role;
+\echo
 
 -- cannot bypass alter-config check by using current_user
 set role supabase_storage_admin;
 alter role supabase_storage_admin set search_path to 'test';
 reset role;
+\echo
 
 -- cannot drop a reserved role
 drop role fake, supabase_storage_admin;
 drop role anon, fake;
+\echo
 
 -- cannot create a reserved role
 create role reserved_but_not_yet_created;
+\echo
 
 -- cannot create a reserved role, even if it exists
 create role anon;
