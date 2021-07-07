06492de5670d8264706e4dba31b59040cd298565
Oliver Rice <github@oliverrice.com>
2021-07-07 06:56:03 -0500

    remove select example because the ast differs between pg 12 and 13

--- test/sql/sql_to_ast.sql
+++ test/sql/sql_to_ast.sql
@@ -1,12 +1,4 @@
 select supa.sql_to_ast($$
-    select name, id, dob
-    from public.uSer
-    where id = 'hello\nworld'
-    limit 1
-$$::text);
-
-
-select supa.sql_to_ast($$
     create table acct(
         id int primary key
     )
