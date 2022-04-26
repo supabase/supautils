9bb45936bc4d3b0e85c75c0fbcd658c48f9d541f
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-04-26 20:07:36 +0800

    feat: privileged extensions custom scripts

--- src/privileged_extensions.h
+++ src/privileged_extensions.h
@@ -5,18 +5,18 @@
 
 extern void
 handle_create_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
-                        PROCESS_UTILITY_PARAMS, CreateExtensionStmt *stmt,
-                        char *privileged_extensions,
-                        char *privileged_extensions_superuser);
+                        PROCESS_UTILITY_PARAMS, char *privileged_extensions,
+                        char *privileged_extensions_superuser,
+                        char *privileged_extensions_custom_scripts_path);
 extern void
 handle_alter_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
-                       PROCESS_UTILITY_PARAMS, AlterExtensionStmt *stmt,
-                       char *privileged_extensions,
-                       char *privileged_extensions_superuser);
+                       PROCESS_UTILITY_PARAMS, char *privileged_extensions,
+                       char *privileged_extensions_superuser,
+                       char *privileged_extensions_custom_scripts_path);
 extern void
 handle_drop_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
-                      PROCESS_UTILITY_PARAMS, DropStmt *stmt,
-                      char *privileged_extensions,
-                      char *privileged_extensions_superuser);
+                      PROCESS_UTILITY_PARAMS, char *privileged_extensions,
+                      char *privileged_extensions_superuser,
+                      char *privileged_extensions_custom_scripts_path);
 
 #endif
