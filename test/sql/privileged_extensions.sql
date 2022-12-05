b01a3b758874f70d4d66acdbd87630672ab7dd59
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-12-05 19:44:45 +0800

    chore: add test

--- test/sql/privileged_extensions.sql
+++ test/sql/privileged_extensions.sql
@@ -7,10 +7,26 @@ set role extensions_role;
 -- can create a privileged extension
 create extension hstore;
 select '1=>2'::hstore;
+
+drop extension hstore;
 \echo
 
--- can run custom scripts
+-- custom scripts are run
 select * from t2;
+
+reset role;
+drop table t2;
+set role extensions_role;
+\echo
+
+-- custom scripts are run even for superusers
+reset role;
+create extension hstore;
+drop extension hstore;
+select * from t2;
+
+drop table t2;
+set role extensions_role;
 \echo
 
 -- cannot create other extensions
