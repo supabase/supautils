de3d5610caf6b8c15322a68a09520324a220f7a0
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-12-05 18:37:43 +0800

    refactor: remove `run_process_utility_hook_as_superuser`
    Use the swith_to_* helper functions instead

--- src/privileged_extensions.h
+++ src/privileged_extensions.h
@@ -8,15 +8,15 @@ handle_create_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
                         PROCESS_UTILITY_PARAMS, char *privileged_extensions,
                         char *privileged_extensions_superuser,
                         char *privileged_extensions_custom_scripts_path);
+
 extern void
 handle_alter_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
                        PROCESS_UTILITY_PARAMS, char *privileged_extensions,
-                       char *privileged_extensions_superuser,
-                       char *privileged_extensions_custom_scripts_path);
+                       char *privileged_extensions_superuser);
+
 extern void
 handle_drop_extension(void (*process_utility_hook)(PROCESS_UTILITY_PARAMS),
                       PROCESS_UTILITY_PARAMS, char *privileged_extensions,
-                      char *privileged_extensions_superuser,
-                      char *privileged_extensions_custom_scripts_path);
+                      char *privileged_extensions_superuser);
 
 #endif
