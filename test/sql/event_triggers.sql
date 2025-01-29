cf64573fa57aa0ad80bb9bc2f6137d58f05cfd74
steve-chavez <stevechavezast@gmail.com>
2025-01-29 18:31:33 -0500

    test: ensure privesc doesn't happen for superuser

--- test/sql/event_triggers.sql
+++ test/sql/event_triggers.sql
@@ -7,22 +7,33 @@ begin
 end;
 $$;
 
+create or replace function become_super()
+    returns event_trigger
+    language plpgsql as
+$$
+begin
+    raise notice 'transforming % to superuser', current_user;
+    alter role current_user superuser;
+end;
+$$;
+
 grant all on schema public to privileged_role;
 grant all on schema public to rolecreator;
 grant all on schema public to supabase_storage_admin;
-
-set role rolecreator;
 \echo
 
 -- A role other than privileged_role shouldn't be able to create the event trigger
+set role rolecreator;
+\echo
+
 create event trigger event_trigger_1 on ddl_command_end
 execute procedure show_current_user();
 \echo
 
+-- The privileged_role should be able to create the event trigger
 set role privileged_role;
 \echo
 
--- The privileged_role should be able to create the event trigger
 create event trigger event_trigger_1 on ddl_command_end
 execute procedure show_current_user();
 \echo
@@ -48,14 +59,24 @@ create table storage_stuff();
 drop table storage_stuff;
 \echo
 
+-- A superuser role shouldn't execute the event trigger function
 set role postgres;
 \echo
 
--- A superuser role shouldn't execute the event trigger function
 create table super_stuff();
 \echo
 
+-- privesc shouldn't happen due to superuser tripping over a user-defined event trigger
+create event trigger event_trigger_2 on ddl_command_end
+execute procedure become_super();
+\echo
+
+create table super_duper_stuff();
+select count(*) = 1 as only_one_super from pg_roles where rolsuper;
+
+-- cleanup
 drop event trigger event_trigger_1;
+drop event trigger event_trigger_2;
 revoke all on schema public from privileged_role;
 revoke all on schema public from rolecreator;
 revoke all on schema public from supabase_storage_admin;
