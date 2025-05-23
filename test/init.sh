2c63fdb61ddfdc1124519c8f569df66333c7b7f9
Bobbie Soedirgo <bobbie@soedirgo.dev>
2025-05-24 02:33:24 +0800

    test: split extension custom scripts tests

--- test/init.sh
+++ test/init.sh
@@ -14,6 +14,6 @@ mkdir -p "$TMPDIR/extension-custom-scripts/autoinc"
 echo 'create extension citext;' > "$TMPDIR/extension-custom-scripts/autoinc/after-create.sql"
 
 # assert both before-create and after-create scripts are run
-mkdir -p "$TMPDIR/extension-custom-scripts/hstore"
-echo 'create table t1();' > "$TMPDIR/extension-custom-scripts/hstore/before-create.sql"
-echo 'drop table t1; create table t2 as values (1);' > "$TMPDIR/extension-custom-scripts/hstore/after-create.sql"
+mkdir -p "$TMPDIR/extension-custom-scripts/fuzzystrmatch"
+echo 'create table t1();' > "$TMPDIR/extension-custom-scripts/fuzzystrmatch/before-create.sql"
+echo 'drop table t1; create table t2 as values (1);' > "$TMPDIR/extension-custom-scripts/fuzzystrmatch/after-create.sql"
