23700768590225366403a0c82fab97a741de11b1
steve-chavez <stevechavezast@gmail.com>
2025-11-27 12:58:57 -0500

    chore: upgrade nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import (builtins.fetchTarball {
-  name = "24.05";
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-  sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  name = "2025-11-13";
+  url = "https://github.com/NixOS/nixpkgs/archive/91c9a64ce2a84e648d0cf9671274bb9c2fb9ba60.tar.gz";
+  sha256 = "sha256:19myp93spfsf5x62k6ncan7020bmbn80kj4ywcykqhb9c3q8fdr1";
 }) {};
 mkShell {
   buildInputs =
