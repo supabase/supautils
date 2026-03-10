6adf34660764818779e54e1a47cf7419f690b09a
steve-chavez <stevechavezast@gmail.com>
2026-03-10 12:34:49 -0500

    test: add loadtest for select

--- bench/init.sql
+++ bench/init.sql
@@ -1,3 +1,4 @@
 create role privileged_role nosuperuser login createrole bypassrls replication;
 grant all on database postgres to privileged_role;
 grant all on schema public to privileged_role;
+grant all on all tables in schema public TO privileged_role;
