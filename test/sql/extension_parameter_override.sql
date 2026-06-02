b57b7454163b3075233527d9ea5792da32a05db7
steve-chavez <stevechavezast@gmail.com>
2026-06-02 17:33:07 -0500

    fix: late validation on supautils.extensions_parameter_overrides
    Same problem as supautils.constrained_extensions fixed on the previous commit,
    also adds tests.

--- test/sql/extension_parameter_override.sql
+++ test/sql/extension_parameter_override.sql
@@ -1,3 +1,13 @@
+-- check json validation works
+alter system set supautils.extensions_parameter_overrides to '';
+alter system set supautils.extensions_parameter_overrides to '[]';
+alter system set supautils.extensions_parameter_overrides to '1';
+alter system set supautils.extensions_parameter_overrides to '{"sslinfo": 123}';
+alter system set supautils.extensions_parameter_overrides to '{"sslinfo": {"schema": {}}}';
+alter system set supautils.extensions_parameter_overrides to '{"sslinfo": {"version": "1.0"}}';
+alter system set supautils.extensions_parameter_overrides to '{"sslinfo": {"schema": 123}}';
+\echo
+
 -- can force sslinfo to be installed in pg_catalog
 create extension sslinfo schema public;
 select extnamespace::regnamespace from pg_extension where extname = 'sslinfo';
